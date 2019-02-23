8cea1617238a579b2df781ddcc78a11ca1c27ea6
yhchoi <cyh8914@gmail.com>
2019-02-23 20:21:27 +0900

    update ipcMain example

--- 03-ipcMain/index.js
+++ 03-ipcMain/index.js
@@ -5,6 +5,7 @@ let win = null;
 app.on('ready', () => {
   win = new BrowserWindow();
   win.loadURL(`file://${__dirname}/index.html`);
+  win.webContents.openDevTools();  // devtools 를 표시
 
   ipcMain.on('async-message', (event, arg) => {
     console.log(arg);  // 'async ping'
