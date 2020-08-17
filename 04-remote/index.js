dee46523106e3ddda0497d4ec2975b3a6da06ce7
yhchoi <cyh8914@gmail.com>
2020-08-17 11:17:25 +0900

    chore: electron upgrade and code clean up

--- 04-remote/index.js
+++ 04-remote/index.js
@@ -3,7 +3,11 @@ const { app, BrowserWindow } = require('electron');
 let main = null;
 
 app.on('ready', () => {
-  main = new BrowserWindow();
-  main.loadURL(`file://${__dirname}/index.html`);
-  main.webContents.openDevTools();
+    main = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+    main.loadURL(`file://${__dirname}/index.html`);
+    main.webContents.openDevTools();
 });
